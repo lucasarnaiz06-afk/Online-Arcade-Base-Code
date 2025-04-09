3e51a22bfc533c7adf3d9527f8c521230b447390
Your Name <you@example.com>
2025-04-09 08:19:29 -0400

    itsdangerous

---
+++ Flask/server/server/app.py
@@ -0,0 +1,70 @@
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import *
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import URLSafeTimedSerializer as Serializer
+import os, datetime
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+mail = Mail(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.datetime.now)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(300), nullable=False)
+
+    def get_reset_token(self):
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token, max_age=1800)['user_id']
+        except:
+            return None
+        return User.query.filter_by(id=user_id).first()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+@app.route('/')
+def home():
+    print("Home")
+    return render_template('index.html')
+
+@app.route('/login')
+def home():
+    print("Login")
+    loginid = input()
+    load_user(loginid)
+    return render_template('index.html')
+
+def send_email(subject, recipients, text_body, html_body):
+     msg = Message(subject, sender=os.getenv('MAIL_USERNAME'), recipients=recipients)
+     msg.body = text_body
+     msg.html = html_body
+     mail.send(msg)
+
+if __name__ == '__main__':
+    print("a")
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=5000, host='0.0.0.0')
